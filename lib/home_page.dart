a00e5090c4be2919991f372528ed5411fbf1f2ed
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-07-25 11:20:13 +0800

    Added home_page Code
    Copied home_page code from source without implementations of provider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,1 +1,65 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFfefcff),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            children: const [],
+          ),
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            children: [
+              GestureDetector(
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border:
+                          Border.all(color: const Color(0xFF716f72), width: 1)),
+                  child: const Icon(Icons.add),
+                ),
+                onTap: () {},
+              ),
+              const Spacer(),
+              Container(
+                height: 60,
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {},
+                          child: const Text(
+                            "Next Page",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          )),
+                      const Spacer(),
+                      const Icon(Icons.skip_next, color: Colors.white)
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+}
